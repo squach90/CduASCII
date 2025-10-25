17d0a156ead485d5cae2ff817dec64b77f3748b7
squach90 <louis.lesniak2@gmail.com>
2025-10-25 18:05:25 +0200

    add color and --debug and replace --nospace

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ stb -> stb_image.h -> @nothings
 file_browser -> file_browser.c/.h -> chip8-cpp
 
 stdio.h
+sys/ioctl.h
+stddef.h
+stdlib.h
+string.h
+dirent.h
+termios.h
+unistd.h
+sys/stat.h
+limits.h
--- src/main.c
+++ src/main.c
@@ -5,17 +5,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) {    
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
-    int space_flag = 0;
-    if (argc > 1 && strcmp(argv[1], "--space") == 0)
-        space_flag = 1;
+    int nospace_flag = 0;
+    int debug_flag = 0;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--nospace") == 0)
+            nospace_flag = 1;
+        else if (strcmp(argv[i], "--debug") == 0)
+            debug_flag = 1;
+    }
+
 
     char *path = select_file();
-    if (!path) return 1;
+    if (!path) {
+        return 1;
+    }
 
     int width, height, channels;
     unsigned char *data = stbi_load(path, &width, &height, &channels, 0);
@@ -25,28 +34,73 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    // Palette
     const char *grayscale = "@%#*+=:-. ";
     int paletteSize = strlen(grayscale);
+    float char_aspect = 2.0f;
+    int effective_cols = w.ws_col;
+    int effective_rows = w.ws_row - 1;
 
-    float scaleX = (float)width / w.ws_col;
-    float scaleY = (float)height / (w.ws_row - 1);
+    if (!nospace_flag) {
+        effective_cols /= 2;
+    }
+
+    float scaleX = (float)width / effective_cols;
+    float scaleY = (float)height / (effective_rows * char_aspect);
     float scale = scaleX > scaleY ? scaleX : scaleY;
 
     int newWidth = width / scale;
     int newHeight = height / scale;
+    if (newWidth < 1) {
+        newWidth = 1;
+    }
+    if (newHeight < 1) {
+        newHeight = 1;
+    }
 
+    int padding = (w.ws_col - (!nospace_flag ? newWidth * 2 : newWidth)) / 2;
+    if (padding < 0) {
+        padding = 0;
+    }
+
+    system("clear");
+    // Draw
     for (int y = 0; y < newHeight; y++) {
+        for (int p = 0; p < padding; p++) {
+            putchar(' ');
+        }
         for (int x = 0; x < newWidth; x++) {
             int srcX = x * scale;
             int srcY = y * scale;
             int i = (srcY * width + srcX) * channels;
-            unsigned char pixelValue = (data[i] + data[i+1] + data[i+2]) / 3;
+
+            unsigned char r = data[i];
+            unsigned char g = data[i+1];
+            unsigned char b = data[i+2];
+            unsigned char pixelValue = (r + g + b) / 3;
+
             int index = (pixelValue * (paletteSize - 1)) / 255;
             char symbol = grayscale[index];
-            putchar(symbol);
-            if (space_flag) putchar(' ');
+
+            printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, symbol);
+
+            if (!nospace_flag) putchar(' ');
         }
         putchar('\n');
+    }
+
+
+    if (debug_flag) {
+        printf("\n=== Pixel Details ===\n");
+        for (int i = 0; i < width * height * channels; i += channels) {
+            unsigned char r = data[i];
+            unsigned char g = data[i + 1];
+            unsigned char b = data[i + 2];
+            unsigned char a = (channels == 4) ? data[i + 3] : 255;
+    
+            printf("Pixel %-5d → R=%3d G=%3d B=%3d A=%3d\n",
+                   i / channels, r, g, b, a);
+        }
     }
 
     stbi_image_free(data);
